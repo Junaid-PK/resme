5383e3f40ac44be31eee3ff5a6619816ad79324b
Junaid-PK <junaidhussnain369@gmail.com>
2024-06-28 07:36:16 +0500

    Inertia Scaffolding: work in progress

---
+++ resources/js/Pages/Test.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Head } from '@inertiajs/vue3'
+
+defineProps({ user: Object })
+</script>
+
+<template>
+  <Layout>
+    <Head title="Welcome" />
+    <h1>Welcome</h1>
+  </Layout>
+</template>
